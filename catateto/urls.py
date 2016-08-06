4a45774a141756190af67606f6eca50603f53965
Henrique Leal <henrique.leal@corp.globo.com>
2016-08-06 10:34:37 -0300

    feature: Implementa estrutura minima usando skeleton css.

--- catateto/urls.py
+++ catateto/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.views import generic
+
+
 urlpatterns = [
+    url(r'^$', generic.TemplateView.as_view(template_name='base.html')),
     url(r'^admin/', admin.site.urls),
 ]
